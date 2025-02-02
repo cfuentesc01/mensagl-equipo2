47bcff44e96be087c12e3bc4d946b0c7501959d3
It's a me, Mario ! <167765641+Mario-Aja@users.noreply.github.com>
2025-02-02 12:49:42 +0100

    Update vpc+grupos.sh
    update security groups for xmpp and fix some misconfigs

--- aws scripts/vpc+grupos.sh
+++ aws scripts/vpc+grupos.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-ALUMNO=X
-# Top-level variables for names and tags
+
+# Personal variables
+ALUMNO="X"
+
+###########################################################################################################
+###########################                      V P C                          ###########################
+###########################################################################################################
+
+# VPC_Variables
 VPC_NAME="vpc-mensagl-2025-${ALUMNO}"
 REGION="us-east-1"
 AVAILABILITY_ZONE1="${REGION}a"
 AVAILABILITY_ZONE2="${REGION}b"
+DESCRIPTION="Mensagl Security group"
+MY_IP="0.0.0.0/0" # Replace with your public IP range or '0.0.0.0/0' for open access
+
 
 # Create VPC and capture its ID
 VPC_ID=$(aws ec2 create-vpc --cidr-block "10.0.0.0/16" --instance-tenancy "default" --tag-specifications "ResourceType=vpc,Tags=[{Key=Name,Value=${VPC_NAME}-vpc}]" --query 'Vpc.VpcId' --output text)
@@ -13,12 +23,9 @@ aws ec2 modify-vpc-attribute --vpc-id $VPC_ID --enable-dns-hostnames
 # Create public and private subnets, capture their IDs
 SUBNET_PUBLIC1=$(aws ec2 create-subnet --vpc-id $VPC_ID --cidr-block "10.0.1.0/24" --availability-zone $AVAILABILITY_ZONE1 --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=${VPC_NAME}-subnet-public1-${AVAILABILITY_ZONE1}}]" --query 'Subnet.SubnetId' --output text)
 aws ec2 modify-subnet-attribute --subnet-id $SUBNET_PUBLIC1 --map-public-ip-on-launch
-
 SUBNET_PUBLIC2=$(aws ec2 create-subnet --vpc-id $VPC_ID --cidr-block "10.0.2.0/24" --availability-zone $AVAILABILITY_ZONE2 --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=${VPC_NAME}-subnet-public2-${AVAILABILITY_ZONE2}}]" --query 'Subnet.SubnetId' --output text)
 aws ec2 modify-subnet-attribute --subnet-id $SUBNET_PUBLIC2 --map-public-ip-on-launch
-
 SUBNET_PRIVATE1=$(aws ec2 create-subnet --vpc-id $VPC_ID --cidr-block "10.0.3.0/24" --availability-zone $AVAILABILITY_ZONE1 --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=${VPC_NAME}-subnet-private1-${AVAILABILITY_ZONE1}}]" --query 'Subnet.SubnetId' --output text)
-
 SUBNET_PRIVATE2=$(aws ec2 create-subnet --vpc-id $VPC_ID --cidr-block "10.0.4.0/24" --availability-zone $AVAILABILITY_ZONE2 --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=${VPC_NAME}-subnet-private2-${AVAILABILITY_ZONE2}}]" --query 'Subnet.SubnetId' --output text)
 
 # Create Internet Gateway and attach to the VPC
@@ -42,7 +49,6 @@ aws ec2 wait nat-gateway-available --nat-gateway-ids $NATGW_ID
 RTB_PRIVATE1=$(aws ec2 create-route-table --vpc-id $VPC_ID --tag-specifications "ResourceType=route-table,Tags=[{Key=Name,Value=${VPC_NAME}-rtb-private1-${AVAILABILITY_ZONE1}}]" --query 'RouteTable.RouteTableId' --output text)
 aws ec2 create-route --route-table-id $RTB_PRIVATE1 --destination-cidr-block "0.0.0.0/0" --nat-gateway-id $NATGW_ID
 aws ec2 associate-route-table --route-table-id $RTB_PRIVATE1 --subnet-id $SUBNET_PRIVATE1
-
 RTB_PRIVATE2=$(aws ec2 create-route-table --vpc-id $VPC_ID --tag-specifications "ResourceType=route-table,Tags=[{Key=Name,Value=${VPC_NAME}-rtb-private2-${AVAILABILITY_ZONE2}}]" --query 'RouteTable.RouteTableId' --output text)
 aws ec2 create-route --route-table-id $RTB_PRIVATE2 --destination-cidr-block "0.0.0.0/0" --nat-gateway-id $NATGW_ID
 aws ec2 associate-route-table --route-table-id $RTB_PRIVATE2 --subnet-id $SUBNET_PRIVATE2
@@ -52,143 +58,173 @@ aws ec2 associate-route-table --route-table-id $RTB_PRIVATE2 --subnet-id $SUBNET
 #aws ec2 describe-nat-gateways --nat-gateway-ids $NATGW_ID
 #aws ec2 describe-route-tables --route-table-ids $RTB_PRIVATE1 $RTB_PRIVATE2
 
+echo "VPC Created !";
+
 
 ###########################################################################################################
-
-
-# Variables
-DESCRIPTION="Security group for EC2 instances"
-MY_IP="0.0.0.0/0" # Replace with your public IP range or '0.0.0.0/0' for open access (not recommended)
-
-SECURITY_GROUP_NAME_PROXYS="${VPC_NAME}-PROXYS"
-SECURITY_GROUP_NAME_XMPP="${VPC_NAME}-XMPP"
-SECURITY_GROUP_NAME_MYSQL="${VPC_NAME}-MYSQL"
-SECURITY_GROUP_NAME_WORDPRESS="${VPC_NAME}-WORDPRESS"
-
+########################                    SECURITY GROUPS                        ########################
+###########################################################################################################
 # Create security group PROXYS
-SG_ID=$(aws ec2 create-security-group \
-  --group-name $SECURITY_GROUP_NAME_PROXYS \
+SG_ID_PROXY=$(aws ec2 create-security-group \
+  --group-name "Proxy-inverso" \
   --description "$DESCRIPTION" \
   --vpc-id $VPC_ID \
-  --tag-specifications "ResourceType=security-group,Tags=[{Key=Name,Value=${SECURITY_GROUP_NAME_PROXYS}}]" \
+  --tag-specifications "ResourceType=security-group,Tags=[{Key=Name,Value="Proxy-inverso"}]" \
   --query 'GroupId' \
   --output text)
 # Add inbound rule to allow SSH
 aws ec2 authorize-security-group-ingress \
-  --group-id $SG_ID \
+  --group-id $SG_ID_PROXY \
   --protocol tcp \
   --port 22 \
   --cidr $MY_IP
 # Add inbound rule to allow HTTP
 aws ec2 authorize-security-group-ingress \
-  --group-id $SG_ID \
+  --group-id $SG_ID_PROXY \
   --protocol tcp \
   --port 80 \
   --cidr $MY_IP
 # Add inbound rule to allow HTTPS
 aws ec2 authorize-security-group-ingress \
-  --group-id $SG_ID \
+  --group-id $SG_ID_PROXY \
   --protocol tcp \
   --port 443 \
   --cidr $MY_IP
-
-
-
 
 
 
 # Create security group XMPP
-SG_ID=$(aws ec2 create-security-group \
-  --group-name $SECURITY_GROUP_NAME_XMPP \
+SG_ID_XMPP=$(aws ec2 create-security-group \
+  --group-name "Servidor-Mensajeria" \
   --description "$DESCRIPTION" \
   --vpc-id $VPC_ID \
-  --tag-specifications "ResourceType=security-group,Tags=[{Key=Name,Value=${SECURITY_GROUP_NAME_XMPP}}]" \
+  --tag-specifications "ResourceType=security-group,Tags=[{Key=Name,Value="Servidor-Mensajeria"}]" \
   --query 'GroupId' \
   --output text)
-# Add inbound rule to allow SSH
+# Add inbound rule to allow 10000
 aws ec2 authorize-security-group-ingress \
-  --group-id $SG_ID \
+  --group-id $SG_ID_XMPP \
   --protocol tcp \
-  --port 22 \
-  --cidr $MY_IP
-# Add inbound rule to allow 5222
-aws ec2 authorize-security-group-ingress \
-  --group-id $SG_ID \
-  --protocol tcp \
-  --port 5222 \
+  --port 1000 \
   --cidr $MY_IP
 # Add inbound rule to allow 5269
 aws ec2 authorize-security-group-ingress \
-  --group-id $SG_ID \
+  --group-id $SG_ID_XMPP \
   --protocol tcp \
   --port 5269 \
   --cidr $MY_IP
-# Add inbound rule to allow MYSQL
+# Add inbound rule to allow 4443
 aws ec2 authorize-security-group-ingress \
-  --group-id $SG_ID \
+  --group-id $SG_ID_XMPP \
   --protocol tcp \
-  --port 3306 \
+  --port 4443 \
   --cidr $MY_IP
-
-
-
-
-
-
-
-
-# Create security group MYSQL
-SG_ID=$(aws ec2 create-security-group \
-  --group-name $SECURITY_GROUP_NAME_MYSQL \
-  --description "$DESCRIPTION" \
-  --vpc-id $VPC_ID \
-  --tag-specifications "ResourceType=security-group,Tags=[{Key=Name,Value=${SECURITY_GROUP_NAME_MYSQL}}]" \
-  --query 'GroupId' \
-  --output text)
-# Add inbound rule to allow SSH
+# Add inbound rule to allow 5281
 aws ec2 authorize-security-group-ingress \
-  --group-id $SG_ID \
+  --group-id $SG_ID_XMPP \
   --protocol tcp \
-  --port 22 \
+  --port 5281 \
   --cidr $MY_IP
-# Add inbound rule to allow MYSQL
+# Add inbound rule to allow 5280
 aws ec2 authorize-security-group-ingress \
-  --group-id $SG_ID \
+  --group-id $SG_ID_XMPP \
   --protocol tcp \
-  --port 3306 \
+  --port 5280 \
   --cidr $MY_IP
-
-
-
-
-
-
-
-# Create security group WORDPRESS
-SG_ID=$(aws ec2 create-security-group \
-  --group-name $SECURITY_GROUP_NAME_WORDPRESS \
-  --description "$DESCRIPTION" \
-  --vpc-id $VPC_ID \
-  --tag-specifications "ResourceType=security-group,Tags=[{Key=Name,Value=${SECURITY_GROUP_NAME_WORDPRESS}}]" \
-  --query 'GroupId' \
-  --output text)
-# Add inbound rule to allow SSH
+# Add inbound rule to allow 5347
 aws ec2 authorize-security-group-ingress \
-  --group-id $SG_ID \
+  --group-id $SG_ID_XMPP \
   --protocol tcp \
-  --port 22 \
+  --port 5347 \
+  --cidr $MY_IP
+# Add inbound rule to allow 5222
+aws ec2 authorize-security-group-ingress \
+  --group-id $SG_ID_XMPP \
+  --protocol tcp \
+  --port 5222 \
   --cidr $MY_IP
 # Add inbound rule to allow HTTP
 aws ec2 authorize-security-group-ingress \
-  --group-id $SG_ID \
+  --group-id $SG_ID_XMPP \
   --protocol tcp \
   --port 80 \
   --cidr $MY_IP
 # Add inbound rule to allow HTTPS
 aws ec2 authorize-security-group-ingress \
-  --group-id $SG_ID \
+  --group-id $SG_ID_XMPP \
+  --protocol tcp \
+  --port 443 \
+  --cidr $MY_IP
+# Add inbound rule to allow SSH
+aws ec2 authorize-security-group-ingress \
+  --group-id $SG_ID_XMPP \
+  --protocol tcp \
+  --port 1000 \
+  --cidr $MY_IP
+# Add inbound rule to allow 12345
+aws ec2 authorize-security-group-ingress \
+  --group-id $SG_ID_XMPP \
+  --protocol tcp \
+  --port 12345 \
+  --cidr $MY_IP
+# Add inbound rule to allow MYSQL
+aws ec2 authorize-security-group-ingress \
+  --group-id $SG_ID_XMPP \
+  --protocol tcp \
+  --port 3306 \
+  --cidr $MY_IP
+
+
+
+# Create security group MYSQL
+SG_ID_MYSQL=$(aws ec2 create-security-group \
+  --group-name "Servidor-SGBD" \
+  --description "$DESCRIPTION" \
+  --vpc-id $VPC_ID \
+  --tag-specifications "ResourceType=security-group,Tags=[{Key=Name,Value="Servidor-SGBD"}]" \
+  --query 'GroupId' \
+  --output text)
+# Add inbound rule to allow SSH
+aws ec2 authorize-security-group-ingress \
+  --group-id $SG_ID_MYSQL \
+  --protocol tcp \
+  --port 22 \
+  --cidr $MY_IP
+# Add inbound rule to allow MYSQL
+aws ec2 authorize-security-group-ingress \
+  --group-id $SG_ID_MYSQL \
+  --protocol tcp \
+  --port 3306 \
+  --cidr $MY_IP
+
+
+
+# Create security group WORDPRESS
+SG_ID_WORDPRESS=$(aws ec2 create-security-group \
+  --group-name "Servidor-ticketing" \
+  --description "$DESCRIPTION" \
+  --vpc-id $VPC_ID \
+  --tag-specifications "ResourceType=security-group,Tags=[{Key=Name,Value="Servidor-ticketing"}]" \
+  --query 'GroupId' \
+  --output text)
+# Add inbound rule to allow SSH
+aws ec2 authorize-security-group-ingress \
+  --group-id $SG_ID_WORDPRESS \
+  --protocol tcp \
+  --port 22 \
+  --cidr $MY_IP
+# Add inbound rule to allow HTTP
+aws ec2 authorize-security-group-ingress \
+  --group-id $SG_ID_WORDPRESS \
+  --protocol tcp \
+  --port 80 \
+  --cidr $MY_IP
+# Add inbound rule to allow HTTPS
+aws ec2 authorize-security-group-ingress \
+  --group-id $SG_ID_WORDPRESS \
   --protocol tcp \
   --port 443 \
   --cidr $MY_IP
 
 
+echo "Sec Groups Created !";
