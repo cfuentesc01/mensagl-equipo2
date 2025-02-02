6f884b53a8f3d495f92822ca49c9a53d931b3906
It's a me, Mario ! <167765641+Mario-Aja@users.noreply.github.com>
2025-02-02 12:52:55 +0100

    Update sshkeys.sh
    add banner KEYS SSH

--- aws scripts/sshkeys.sh
+++ aws scripts/sshkeys.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-# Key pair SSH
+###########################################################################################################
+#########################                      KEYS SSH                          ##########################
+###########################################################################################################
 
-ALUMNO=X
+# Key pair SSH
+ALUMNO="X"
 KEY_NAME="ssh-mensagl-2025-${ALUMNO}"
 
 aws ec2 create-key-pair \
   --key-name "${KEY_NAME}" \
   --query "KeyMaterial" \
   --output text > ${KEY_NAME}.pem
- 
+
+echo "SSH KEYS !";
