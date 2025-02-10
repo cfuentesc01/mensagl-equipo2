e0c65450797be1a29227585c95969a653a678dfc
MortinLG <mlianog@educantabria.es>
2025-02-10 10:44:58 +0100

    Update rsync 2AM.sh

--- user-data/rsync 2AM.sh
+++ user-data/rsync 2AM.sh
@@ -9,7 +9,6 @@ SSH_KEY="/home/osboxes/.ssh/id_rsa"  # Ruta de la clave SSH
 MYSQL_DATA_DIR="/var/lib/mysql"
 LOGFILE="/var/log/backup_mysql.log"
 DUMP_FILE="/tmp/${DB_NAME}-full-dump.sql"  # Ruta de los archivos de datos de MySQL
-CRON_JOB="0 2 * * * /path/to/this/script.sh >> /var/log/backup_cron.log 2>&1"
 
 # Marca temporal para diferenciar los respaldos
 DATE=$(date +"%Y%m%d%H%M")
@@ -17,49 +16,47 @@ DATE=$(date +"%Y%m%d%H%M")
 # Crear directorio de backup si no existe
 mkdir -p "$BACKUP_DIR/$DB_NAME"
 
-function setup_cron_job() {
-  crontab -l | grep -q "$CRON_JOB"
-  if [ $? -ne 0 ]; then
-    echo "Configurando cron para ejecutar el script diariamente a las 2:00 AM..."
-    (crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
-    echo "Tarea programada creada."
-  else
-    echo "La tarea cron ya está configurada."
-  fi
-}
-
 function perform_backup() {
-  echo "=== Iniciando backup incremental de la base de datos '$DB_NAME' ==="
+# Si es el primer backup, hacemos un dump completo
+if [ ! -f "$BACKUP_DIR/$DB_NAME/last_backup" ]; then
+    echo "== Realizando un respaldo completo de la base de datos '$DB_NAME' en el maestro =="
+    ssh -i "$SSH_KEY" "$MAESTRO_USER@$MAESTRO_IP" \
+        "sudo mysqldump -u root --databases $DB_NAME --single-transaction --quick --lock-tables=false > $DUMP_FILE"
 
-  # Si es el primer backup, hacemos un dump completo
-  if [ ! -f "$BACKUP_DIR/$DB_NAME/last_backup" ]; then
-      echo "== Realizando un respaldo completo de la base de datos '$DB_NAME' en el maestro =="
-      ssh -i "$SSH_KEY" "$MAESTRO_USER@$MAESTRO_IP" \
-          "sudo mysqldump -u root --databases $DB_NAME --single-transaction --quick --lock-tables=false > $DUMP_FILE"
+    # Transferir el volcado completo al esclavo
+    scp -i "$SSH_KEY" "$MAESTRO_USER@$MAESTRO_IP:/tmp/$DB_NAME-full-dump.sql" "$BACKUP_DIR/$DB_NAME/"
+    echo "Backup completo realizado: $(date)" >> "$LOGFILE"
+    touch "$BACKUP_DIR/$DB_NAME/last_backup"
+else
+    # Realizar un respaldo incremental de los archivos de datos usando rsync
+    echo "== Realizando backup incremental de los archivos de datos de MySQL de '$DB_NAME' =="
 
-      # Transferir el volcado completo al esclavo
-      scp -i "$SSH_KEY" "$MAESTRO_USER@$MAESTRO_IP:/tmp/$DB_NAME-full-dump.sql" "$BACKUP_DIR/$DB_NAME/"
-      echo "Backup completo realizado: $(date)" >> "$LOGFILE"
-      touch "$BACKUP_DIR/$DB_NAME/last_backup"
-  else
-      # Realizar un respaldo incremental de los archivos de datos usando rsync
-      echo "== Realizando backup incremental de los archivos de datos de MySQL de '$DB_NAME' =="
-      mkdir -p "$BACKUP_DIR/$DB_NAME/incremental/$DATE"
+    sudo mkdir -p "$BACKUP_DIR/$DB_NAME/incremental/$DATE"
 
-      sshpass -p 'osboxes.org' rsync -avz --delete -e "ssh -i $SSH_KEY" --rsync-path="sudo rsync" \
-        "$MAESTRO_USER@$MAESTRO_IP:$MYSQL_DATA_DIR/$DB_NAME/" \
-        "$BACKUP_DIR/$DB_NAME/incremental/$DATE/"
+    # Usamos rsync para copiar los archivos de datos que han cambiado desde el último backup
+    sshpass -p 'osboxes.org' sudo rsync -avz --delete -e "ssh -i $SSH_KEY" --rsync-path="sudo rsync" "$MAESTRO_USER@$MAESTRO_IP:$MYS>
+    if [ $? -eq 0 ]; then
+    echo "Backup incremental exitoso para '$DB_NAME': $DATE" >> "$LOGFILE"
+        else
+    echo "Error en el backup incremental: $DATE" >> "$LOGFILE"
+        fi
+fi
 
-      if [ $? -eq 0 ]; then
-          echo "Backup incremental exitoso para '$DB_NAME': $DATE" >> "$LOGFILE"
-      else
-          echo "Error en el backup incremental: $DATE" >> "$LOGFILE"
-      fi
-  fi
-
-  echo "=== Backup finalizado para la base de datos '$DB_NAME' ==="
+echo "=== Backup finalizado para la base de datos '$DB_NAME' ==="
 }
 
 # Ejecutar configuración de cron y backup
-setup_cron_job
 perform_backup
+
+sudo visudo
+
+osboxes ALL=(ALL) NOPASSWD: /home/osboxes/backup_maestro.sh
+
+sudo chmod +x "ruta crear"
+
+sudo crontab -e
+
+0 2 * * * /home/osboxes/backup_maestro.sh >> /var/log/backup_mysql.log 2>&1
+
+sudo crontab -l
+
