b6b0e578c38cc46182df3e6bd793c1f066508f99
It's a me, Mario ! <167765641+majam02@users.noreply.github.com>
2025-02-12 09:50:41 +0100

    Update mysql.sh

--- user-data/vms/mysql.sh
+++ user-data/vms/mysql.sh
@@ -16,7 +16,6 @@ export CONFIG_FILE="/etc/mysql/mysql.conf.d/mysqld.cnf"
 # Update MySQL Configuration using awk
 awk -i inplace '
     /^bind-address/ { $0="bind-address = 0.0.0.0" }
-    /^mysqlx-bind-address/ { $0="mysqlx-bind-address = 127.0.0.1" }
     /^# server-id/ { $0="server-id = 1" }
     /^# log_bin/ { $0="log_bin = /var/log/mysql/mysql-bin.log" }
     { print }
@@ -27,11 +26,21 @@ cat "$CONFIG_FILE"
 
 #sudo systemctl restart mysql
 
+
+
 # Secure MySQL User Creation
 sudo mysql -e "CREATE USER IF NOT EXISTS '${DB_USERNAME}'@'%' IDENTIFIED WITH mysql_native_password BY '${DB_PASSWORD}';"
 sudo mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${DB_USERNAME}'@'%' WITH GRANT OPTION;"
 sudo mysql -e "GRANT REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO '${DB_USERNAME}'@'%';"
 sudo mysql -e "FLUSH PRIVILEGES;"
+
+sleep 40
+
+echo "Obteniendo información del maestro..."
+MASTER_STATUS=$(mysql -h "10.0.3.20" -u "${DB_USERNAME}" -p"${DB_PASSWORD}" -e "SHOW MASTER STATUS\G" 2>/dev/null)
+BINLOG_FILE=$(echo "$MASTER_STATUS" | grep "File:" | awk '{print $2}')
+BINLOG_POSITION=$(echo "$MASTER_STATUS" | grep "Position:" | awk '{print $2}')
+echo "Archivo binlog: $BINLOG_FILE, Posición: $BINLOG_POSITION"
 
 mysql -u root <<SQL
 CHANGE MASTER TO
