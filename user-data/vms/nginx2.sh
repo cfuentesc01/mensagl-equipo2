bd1e57bf97056997a587f93b1939f7f3fe1cde58
It's a me, Mario ! <167765641+majam02@users.noreply.github.com>
2025-02-12 09:40:56 +0100

    Update nginx2.sh

--- user-data/vms/nginx2.sh
+++ user-data/vms/nginx2.sh
@@ -48,7 +48,6 @@ sudo certbot certonly  --non-interactive \
 
 cat <<EOF > /etc/nginx/sites-available/proxy_site
 upstream backend_servers {
-    ip_hash;
     server 10.0.4.100:443;
     server 10.0.4.200:443;
 }
